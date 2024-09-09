ad25f7a87fd94d6bd9008c8804001e60ffb0478d
Frederic <bilf0901@usherbrooke.ca>
2024-09-09 14:41:58 -0400

    fred: ne pas mettre 2 fois la meme ligne dans le .f merci... envoy/ de mon iphone

--- gei815-amorce-problematique/verif/tb_sva_bindings.f
+++ gei815-amorce-problematique/verif/tb_sva_bindings.f
@@ -11,4 +11,3 @@ $VERIF_ROOT/assertions/CRC8_Bindings.sv
 $VERIF_ROOT/assertions/RegisterCoverage.sv
 $VERIF_ROOT/assertions/TDCCoverage.sv
 $VERIF_ROOT/assertions/PacketMergerCoverage.sv
-$VERIF_ROOT/assertions/RegisterCoverage.sv
