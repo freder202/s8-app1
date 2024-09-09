6368f4894e81d859dd62af4530714847391bd194
Frederic <bilf0901@usherbrooke.ca>
2024-09-09 14:29:04 -0400

    fred: lab4 partie 1

--- gei815-amorce-problematique/verif/tb_sva_bindings.f
+++ gei815-amorce-problematique/verif/tb_sva_bindings.f
@@ -11,3 +11,4 @@ $VERIF_ROOT/assertions/CRC8_Bindings.sv
 $VERIF_ROOT/assertions/RegisterCoverage.sv
 $VERIF_ROOT/assertions/TDCCoverage.sv
 $VERIF_ROOT/assertions/PacketMergerCoverage.sv
+$VERIF_ROOT/assertions/RegisterCoverage.sv
